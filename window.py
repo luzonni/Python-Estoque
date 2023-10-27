e099f83fd0a79921e35a10a5c7678aefd1253137
Francisco Neto <68744001+Francisco-Neto13@users.noreply.github.com>
2023-10-27 13:19:08 -0300

    Coloquei a janela para ficar com uma divisão: tabela na esquerda e entrada e botões na direita. Também adicionei para que as informações dentro da janela fiquem proporcionais a mesma

--- window.py
+++ window.py
@@ -2,8 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 import tkinter.font as tkFont
 
-
-# Função para adicionar um valor à tabela
 def adicionar_valor():
     valor1 = valor1_entry.get()
     valor2 = valor2_entry.get()
@@ -17,28 +15,39 @@ def adicionar_valor():
         valor3_entry.delete(0, 'end')
         valor4_entry.delete(0, 'end')
 
-
-# Função para remover um valor selecionado da tabela
 def remover_valor():
     selected_item = tree.selection()
     if selected_item:
         tree.delete(selected_item)
-        
+
 def recuperar_valores():
     for item in tree.get_children():
         valores = tree.item(item)['values']
         print(valores)
 
-# Crie uma instância da janela principal
+# Instância da janela principal
 janela = tk.Tk()
 janela.title("Tabela de Valores")
 
+# Cor da Janela
 janela.configure(bg='#363e50')
-font = tkFont.Font(family='Arial',size=14,weight='bold')
 
+# Configurar o tamanho mínimo da janela
+janela.minsize(400, 300)
+
+# Configurar a fonte
+font = tkFont.Font(family='Arial', size=14, weight='bold')
+
+# Feito para colocar a tabela na esquerda
+frame_esquerda = tk.Frame(janela)
+frame_esquerda.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+
+# Feito para colocar as entradas e os botões na direita
+frame_direita = tk.Frame(janela)
+frame_direita.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
 
 # Crie um widget Treeview para a tabela
-tree = ttk.Treeview(janela, columns=('ID', 'Nome', 'Quantidade', 'TAG'), show='headings')
+tree = ttk.Treeview(frame_esquerda, columns=('ID', 'Nome', 'Quantidade', 'TAG'), show='headings')
 tree.heading('#1', text='ID')
 tree.heading('#2', text='Nome')
 tree.heading('#3', text='Quantidade')
@@ -46,34 +55,42 @@ tree.heading('#4', text='TAG')
 
 tree.column('#0', width=0, stretch=tk.NO)
 
-tree.pack()
+
+tree.pack(fill=tk.BOTH, expand=True) #o fill=tk.BOTH, expand=true serve para preencher e expandir para ajustar ao tamanho
+
+
+# Isso aqui serve para configurar  o peso das colunas e linhas para que elas se ajustem à janela
+janela.columnconfigure(0, weight=1)
+janela.columnconfigure(1, weight=1)
+janela.rowconfigure(0, weight=1)
 
 # Crie entradas para adicionar valores
-valor1_label = tk.Label(janela, text='ID:',font=font)
+valor1_label = tk.Label(frame_direita, text='ID:')
 valor1_label.pack()
-valor1_entry = tk.Entry(janela)
-valor1_entry.pack()
+valor1_entry = tk.Entry(frame_direita)
+valor1_entry.pack(fill=tk.X)
 
-valor2_label = tk.Label(janela, text='Nome:',font=font)
+valor2_label = tk.Label(frame_direita, text='Nome:')
 valor2_label.pack()
-valor2_entry = tk.Entry(janela)
-valor2_entry.pack()
+valor2_entry = tk.Entry(frame_direita)
+valor2_entry.pack(fill=tk.X)
 
-valor3_label = tk.Label(janela, text='Quantidade:',font=font)
+valor3_label = tk.Label(frame_direita, text='Quantidade:')
 valor3_label.pack()
-valor3_entry = tk.Entry(janela)
-valor3_entry.pack()
+valor3_entry = tk.Entry(frame_direita)
+valor3_entry.pack(fill=tk.X)
 
-valor4_label = tk.Label(janela, text='TAG:',font=font)
+valor4_label = tk.Label(frame_direita, text='TAG:')
 valor4_label.pack()
-valor4_entry = tk.Entry(janela)
-valor4_entry.pack()
+valor4_entry = tk.Entry(frame_direita)
+valor4_entry.pack(fill=tk.X)
+
+# O fill=tk.x serve para peencher horizontalmente
 
 # Crie botões para adicionar e remover valores
-tk.Button(janela, text="Adicionar Valor", command=adicionar_valor).pack()
-tk.Button(janela, text="Remover Valor Selecionado", command=remover_valor).pack()
-tk.Button(janela, text="Print Valores", command=recuperar_valores).pack()
+tk.Button(frame_direita, text="Adicionar Valor", command=adicionar_valor).pack()
+tk.Button(frame_direita, text="Remover Valor Selecionado", command=remover_valor).pack()
+tk.Button(frame_direita, text="Print Valores", command=recuperar_valores).pack()
 
 # Inicie o loop principal da interface gráfica
 janela.mainloop()
-
