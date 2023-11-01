95e974642c3d79f5dc839cad051fa2bce204cdae
Francisco Neto <68744001+Francisco-Neto13@users.noreply.github.com>
2023-11-01 10:14:32 -0300

    Atualização da janela

--- window.py
+++ window.py
@@ -1,19 +1,20 @@
 import tkinter as tk
+from tkinter import * 
 from tkinter import ttk
 import tkinter.font as tkFont
+from Tagger import getTags
+
+
+TAGS:list = getTags("Tags.txt")
 
 def adicionar_valor():
-    valor1 = valor1_entry.get()
-    valor2 = valor2_entry.get()
-    valor3 = valor3_entry.get()
-    valor4 = valor4_entry.get()
-
-    if valor1 and valor2 and valor3 and valor4:
-        tree.insert('', 'end', values=(valor1, valor2, valor3, valor4))
-        valor1_entry.delete(0, 'end')
-        valor2_entry.delete(0, 'end')
-        valor3_entry.delete(0, 'end')
-        valor4_entry.delete(0, 'end')
+    valor1 = entry_nome.get()
+    valor2 = entry_quantidade.get()
+    valor3 = TAGS[int(entry_tag.curselection()[0])]
+    if valor1 and valor2 and valor3:
+        tree.insert('', 'end', values=(0, valor1, valor2, valor3))
+        entry_nome.delete(0, 'end')
+        entry_quantidade.delete(0, 'end')
 
 def remover_valor():
     selected_item = tree.selection()
@@ -45,8 +46,8 @@ frame_direita.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
 
 
 # Crie um widget Treeview para a tabela
-tree = ttk.Treeview(frame_esquerda, columns=('CATEGORIA', 'NOME', 'QUANTIDADE', 'TAG'), show='headings')
-tree.heading('#1', text='CATEGORIA')
+tree = ttk.Treeview(frame_esquerda, columns=('ID', 'NOME', 'QUANTIDADE', 'TAG'), show='headings')
+tree.heading('#1', text='ID')
 tree.heading('#2', text='NOME')
 tree.heading('#3', text='QUANTIDADE')
 tree.heading('#4', text='TAG')
@@ -66,36 +67,35 @@ janela.columnconfigure(1, weight=1)
 janela.rowconfigure(0, weight=1)
 
 # Crie entradas para adicionar valores
-valor1_label = tk.Label(frame_direita, text='CATEGORIA:', font=framedireitafont)
-valor1_label.pack()
-valor1_entry = tk.Entry(frame_direita)
-valor1_entry.pack(fill=tk.X)
 
-valor2_label = tk.Label(frame_direita, text='NOME:', font=framedireitafont)
-valor2_label.pack()
-valor2_entry = tk.Entry(frame_direita)
-valor2_entry.pack(fill=tk.X)
+label_nome = tk.Label(frame_direita, text='NOME:', font=framedireitafont)
+label_nome.pack()
+entry_nome = tk.Entry(frame_direita)
+entry_nome.pack(fill=tk.X)
 
-valor3_label = tk.Label(frame_direita, text='QUANTIDADE:', font=framedireitafont)
-valor3_label.pack()
-valor3_entry = tk.Entry(frame_direita)
-valor3_entry.pack(fill=tk.X)
+label_quantidade = tk.Label(frame_direita, text='QUANTIDADE:', font=framedireitafont)
+label_quantidade.pack()
+entry_quantidade = tk.Entry(frame_direita)
+entry_quantidade.pack(fill=tk.X)
 
-valor4_label = tk.Label(frame_direita, text='TAG:', font=framedireitafont)
-valor4_label.pack()
-valor4_entry = tk.Entry(frame_direita)
-valor4_entry.pack(fill=tk.X)
+label_tag = tk.Label(frame_direita, text='TAG:', font=framedireitafont)
+label_tag.pack()
+entry_tag = tk.Listbox(frame_direita)
+my_list = TAGS
+for item in my_list:
+    entry_tag.insert(END, item)
+entry_tag.pack(fill=tk.X)
 
 #Criação e personalização dos botões
 
 # Botão Adicionar
 botao_adicionar = tk.Button(frame_direita, text="Adicionar Valor", command=adicionar_valor)
-botao_adicionar.config(bg="green", fg="White", width=15)
+botao_adicionar.config(bg="#8ab0bd", fg="black", width=15)
 botao_adicionar.pack(pady=5)
 
 # Botão Remover
 botao_remover = tk.Button(frame_direita, text="Remover Valor", command=remover_valor)
-botao_remover.config(bg="red", fg="White", width=15)
+botao_remover.config(bg="#8ab0bd", fg="black", width=15)
 botao_remover.pack(pady=5)
 
 # Botão Editar
@@ -105,8 +105,8 @@ botao_remover.pack(pady=5)
 
 
 # Botão Mostrar Valores
-botao_mostrar = tk.Button(frame_direita, text="Mostrar Valores", command=recuperar_valores)
-botao_mostrar.config(bg="yellow", width=15)
+botao_mostrar = tk.Button(frame_direita, text="Editar Valores", command=recuperar_valores)
+botao_mostrar.config(bg="#8ab0bd", fg="black", width=15)
 botao_mostrar.pack(pady=5)
 
 # estilo da Treeview para as colunas
@@ -116,10 +116,6 @@ column_style.configure("Treeview.Heading", font=('Arial', 8), background="#12171
 # estilo da Treeview para as linhas
 row_style = ttk.Style()
 row_style.configure("Treeview", rowheight=25, background="#bbd5de", font=('Arial', 8))
-
-# Obs: Tentei fazer com que as linhas e colunas ficassem com espaçamento, cor e qualquer coisa do tipo diferente, mas
-# aparentemente não deu certo. Se souber a resposta, seria interessante alterar isso. Vou deixar ambos no código, pois
-# não faz diferença. O Único que funciona é o row_style
 
 # Inicie o loop principal da interface gráfica
 janela.mainloop()
