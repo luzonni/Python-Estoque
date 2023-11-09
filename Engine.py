25009ed5a8ea4e1d2e55d167cdad58e7ac4bf282
Zonni <lucas.z.200375@gmail.com>
2023-11-09 19:56:22 -0300

    Config frame add

--- Engine.py
+++ Engine.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import * 
 from tkinter import ttk
+from tkinter import messagebox
 import tkinter.font as tkFont
 from Produto import Produto
 from Estoque import Estoque
@@ -29,13 +30,16 @@ class Engine:
         self.__estoque = newEstoque
     
     def configWindow(self, janela:Tk):
+        janela.resizable(width=False, height=False)
+        janela.columnconfigure(0, weight=1)
+        janela.columnconfigure(1, weight=1)
+        janela.rowconfigure(0, weight=1)
+        janela.protocol("WM_DELETE_WINDOW", self.close)
         framedireitafont = tkFont.Font(family='Arial', size=10, weight='bold')
+        
+        # Frame da tabela
         frame_esquerda = tk.Frame(janela)
         frame_esquerda.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
-        frame_direita = tk.Frame(janela)
-        frame_direita.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
-        frame_direita = tk.Frame(janela)
-        frame_direita.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
         self.tree = ttk.Treeview(frame_esquerda, columns=('ID', 'NOME', 'QUANTIDADE', 'TAG'), show='headings')
         self.tree.heading('#1', text='ID')
         self.tree.heading('#2', text='NOME')
@@ -45,45 +49,77 @@ class Engine:
         self.tree.column('#2', width=100)
         self.tree.column('#3', width=100)
         self.tree.column('#4', width=100)
+        row_style = ttk.Style()
+        row_style.configure("Treeview", rowheight=25, background="#bbd5de", font=('Arial', 8))
+        column_style = ttk.Style()
+        column_style.configure("Treeview.Heading", font=('Arial', 8), background="#121718")
         self.tree.pack(fill=tk.BOTH, expand=True)
+         
+        # Frame de manipulação
+        frame_meio = tk.Frame(janela)
+        frame_meio.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
+        frame_meio = tk.Frame(janela)
+        frame_meio.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
         if not self.__estoque.isEmpty():
             produtos = self.__estoque.getList()
             for produto in produtos:
                 self.tree.insert('', 'end', values=(produto.id, produto.nome, produto.quantidade, TAGS[produto.tagId]))
-        janela.columnconfigure(0, weight=1)
-        janela.columnconfigure(1, weight=1)
-        janela.rowconfigure(0, weight=1)
-        label_nome = tk.Label(frame_direita, text='NOME:', font=framedireitafont)
+        label_nome = tk.Label(frame_meio, text='NOME:', font=framedireitafont)
         label_nome.pack()
-        self.entry_nome = tk.Entry(frame_direita)
+        self.entry_nome = tk.Entry(frame_meio)
         self.entry_nome.pack(fill=tk.X)
-        label_quantidade = tk.Label(frame_direita, text='QUANTIDADE:', font=framedireitafont)
+        label_quantidade = tk.Label(frame_meio, text='QUANTIDADE:', font=framedireitafont)
         label_quantidade.pack()
-        self.entry_quantidade = tk.Entry(frame_direita)
+        self.entry_quantidade = tk.Entry(frame_meio)
         self.entry_quantidade.pack(fill=tk.X)
-        label_tag = tk.Label(frame_direita, text='TAG:', font=framedireitafont)
+        label_tag = tk.Label(frame_meio, text='TAG:', font=framedireitafont)
         label_tag.pack()
-        self.entry_tag = tk.Listbox(frame_direita)
+        self.entry_tag = tk.Listbox(frame_meio)
         my_list = TAGS
         for item in my_list:
             self.entry_tag.insert(END, item)
         self.entry_tag.pack(fill=tk.X)
-        botao_adicionar = tk.Button(frame_direita, text="Adicionar Valor", command=self.adicionar_valor)
+        botao_adicionar = tk.Button(frame_meio, text="Adicionar Valor", command=self.adicionar_valor)
         botao_adicionar.config(bg="#8ab0bd", fg="black", width=15)
         botao_adicionar.pack(pady=5)
-        botao_remover = tk.Button(frame_direita, text="Remover Valor", command=self.remover_valor)
+        botao_remover = tk.Button(frame_meio, text="Remover Valor", command=self.remover_valor)
         botao_remover.config(bg="#8ab0bd", fg="black", width=15)
         botao_remover.pack(pady=5)
-        botao_mostrar = tk.Button(frame_direita, text="Editar Valores", command=self.recuperar_valores)
+        botao_mostrar = tk.Button(frame_meio, text="Editar Valores", command=self.recuperar_valores)
         botao_mostrar.config(bg="#8ab0bd", fg="black", width=15)
         botao_mostrar.pack(pady=5)
-        column_style = ttk.Style()
-        column_style.configure("Treeview.Heading", font=('Arial', 8), background="#121718")
-        row_style = ttk.Style()
-        row_style.configure("Treeview", rowheight=25, background="#bbd5de", font=('Arial', 8))
-        janela.protocol("WM_DELETE_WINDOW", self.close)
-        janela.mainloop()
         
+        # Frame de Configuração
+        frame_direita = tk.Frame(janela)
+        frame_direita.grid(row=0, column=2, padx=10, pady=10, sticky='nsew')
+        tk.Label(frame_direita, text='___ Configurações ___', font=framedireitafont, bg="#7d7c7c", ).pack()
+        tk.Label(frame_direita, text='Limpar lista de Tags:', font=framedireitafont, bg="#7d7c7c").pack()
+        botao_remover_tags = tk.Button(frame_direita, text="Limpar", command=self.dellTagFile)
+        botao_remover_tags.config(bg="#8a070d")
+        botao_remover_tags.pack(fill=tk.X)
+        tk.Label(frame_direita, text='Reiniciar tabela:', font=framedireitafont, bg="#7d7c7c").pack()
+        botao_remover_items = tk.Button(frame_direita, text="Limpar", command=self.dellallItemsFile)
+        botao_remover_items.config(bg="#8a070d")
+        botao_remover_items.pack(fill=tk.X)
+        frame_direita.config(bg="#7d7c7c")
+        
+        
+        janela.mainloop()
+    
+    def dellTagFile(self):
+        resposta = messagebox.askyesno("Confirmação", "Deseja realmente apagar todas as Tags? Isso fará TODOS os itens da lista serem removidos")
+        if resposta:
+            print("Deletar Tags!")
+        else: 
+            print("Ação cancelada!")
+            
+    def dellallItemsFile(self):
+        resposta = messagebox.askyesno("Confirmação", "Deseja realmente apagar todas as Tags? Isso fará TODOS os itens da lista serem removidos")
+        if resposta:
+            print("Deletar Items!")
+        else: 
+            print("Ação cancelada!")
+    
     def adicionar_valor(self):
         id:int = len(self.__estoque.getList())
         nome:str = self.entry_nome.get()
